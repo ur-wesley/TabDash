c32a9e4d51268aa5c5e40e57a9f5edfdeed91a8f
Wesley <wes.urb@gmail.com>
2022-11-29 11:45:19 +0100

    remove comments

--- extension/src/components/widgets/backdrop.tsx
+++ extension/src/components/widgets/backdrop.tsx
@@ -1,13 +1,10 @@
-import { Component } from "solid-js";
+import { Component } from 'solid-js';
 
 const Backdrop: Component<Prop> = (props) => {
-  // const blur = `backdrop-blur-[${props.blur}px]`;
-  // const saturate = `backdrop-saturate-[${props.saturate}]`;
-  // const brightness = `backdrop-brightness-[${props.brightness}]`;
   return (
     <div
       style={{
-        "backdrop-filter": `blur(${props.blur}) saturate(${props.saturate}) brightness(${props.brightness})`,
+        'backdrop-filter': `blur(${props.blur}) saturate(${props.saturate}) brightness(${props.brightness})`,
       }}
       class={`absolute z-10 left-0 top-0 w-full h-full backdrop-filter`}
     />
