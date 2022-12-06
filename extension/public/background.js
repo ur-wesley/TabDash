c60ce0625bb8480bef4f98cb3fa842ce7cd1ceac
Wesley <wes.urb@gmail.com>
2022-12-06 17:45:55 +0100

    formatting

--- extension/public/background.js
+++ extension/public/background.js
@@ -1,23 +1,22 @@
 const isOnChrome = navigator.userAgent.includes('Chrome');
-  const newTab = () => chrome.tabs.create({ url: 'chrome://newtab' });
-  const url = `https://tabdash.wesley.fyi/`
-  chrome.runtime.onInstalled.addListener(function (d) {
+const newTab = () => chrome.tabs.create({ url: 'chrome://newtab' });
+const url = `https://tabdash.wesley.fyi/`;
+chrome.runtime.onInstalled.addListener(function (d) {
   if (d?.reason === 'install') {
     const key = crypto.randomUUID().split('-')[0] + '_' + Date.now();
     chrome.storage.local.set({ key });
     chrome.runtime.setUninstallURL(
       url +
-      (isOnChrome ? 'api/chrome/goodbye' : 'api/firefox/goodbye') +
-      '?key=' +
-      key
+        (isOnChrome ? 'api/chrome/goodbye' : 'api/firefox/goodbye') +
+        '?key=' +
+        key
     );
     fetch(
       url +
-      (isOnChrome ? 'api/chrome/install' : 'api/firefox/install') +
-      '?key=' +
-      key
-      );
-      newTab();
-    }
-  });
-  
+        (isOnChrome ? 'api/chrome/install' : 'api/firefox/install') +
+        '?key=' +
+        key
+    );
+    newTab();
+  }
+});
