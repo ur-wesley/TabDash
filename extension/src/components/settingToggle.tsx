21def0d238731ef190c5ca44ef8f74e96602a5dd
Wesley <wes.urb@gmail.com>
2022-12-06 12:39:34 +0100

    added background and updated icon animation; closes #3

--- extension/src/components/settingToggle.tsx
+++ extension/src/components/settingToggle.tsx
@@ -22,21 +22,23 @@ const SettingToggle: Component<Prop> = (props) => {
   });
   return (
     <>
-      <span
-        class={`text-gray-400 dark:text-gray-100 i-mdi-cog transition hover:text-orange-200 group absolute right-2 bottom-2 z-40 p-3 cursor-pointer rounded-full transition duration-300 hover:rotate-45 ${
-          showSetting() ? 'bg-gray' : null
-        }`}
-        onClick={() => {
-          setShowSetting(!showSetting());
-          const el = document.getElementById('content');
-          if (showSetting()) {
-            el!.style.transform = 'translate(-10%,0)';
-            el!.style.transition = 'all 300ms';
-          } else el!.style.transform = '';
-        }}
-      />
+      <div class='z-40 widget absolute right-2 bottom-2 grid place-content-center overflow-hidden p-1'>
+        <span
+          class={`i-mdi-cog transition cursor-pointer p-4 transition ease-in-out duration-300 bg-gray-800 dark:bg-gray-200 outline-transparent border-red ${
+            showSetting() ? 'rotate-90' : 'hover:rotate-12'
+          }`}
+          onClick={() => {
+            setShowSetting(!showSetting());
+            const el = document.getElementById('content');
+            if (showSetting()) {
+              el!.style.transform = 'translate(-10%,0)';
+              el!.style.transition = 'all 300ms';
+            } else el!.style.transform = '';
+          }}
+        />
+      </div>
       <Show when={showSetting()}>
-        <div class='absolute top-0 left-0 w-screen h-screen backdrop-brightness-50'></div>
+        <div class='absolute top-0 left-0 w-screen h-screen backdrop-brightness-80'></div>
       </Show>
       <aside
         ref={settingPanel!}
