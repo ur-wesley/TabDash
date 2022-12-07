bcb3e5885796f9473b419b63472a134dae1da6ea
Wesley <wes.urb@gmail.com>
2022-12-07 19:03:56 +0100

    init

---
+++ extension/src/components/controls/tabs.tsx
@@ -0,0 +1,34 @@
+import { Component, createSignal, For, Show } from 'solid-js';
+
+const Tabs: Component<Props> = (props) => {
+  const [activeTab, setActiveTab] = createSignal(props.activeTab || 0);
+  return (
+    <div class='flex flex-col gap-2'>
+      <div class='surface-base p-2 flex text-center rounded-xl'>
+        <For each={props.tabs}>
+          {(tab, index) => (
+            <span
+              class={`p-1 rounded-md grow w-full cursor-pointer ${
+                activeTab() == index()
+                  ? 'text-white bg-blue-500'
+                  : 'text-blue-500 bg-transparent'
+              }`}
+              onclick={() => setActiveTab(index())}
+            >
+              {tab}
+            </span>
+          )}
+        </For>
+      </div>
+      {props.children[activeTab()]}
+    </div>
+  );
+};
+
+export default Tabs;
+
+interface Props {
+  activeTab?: number;
+  tabs: string[];
+  children: any[];
+}
---
+++ extension/src/components/controls/textarea.tsx
@@ -0,0 +1,57 @@
+import { Component, createSignal, Show } from 'solid-js';
+
+const Textarea: Component<Prop> = (props) => {
+  const [error, setError] = createSignal(false);
+  return (
+    <div class='flex items-center justify-between gap-4 w-full z-20 my-4'>
+      <Show when={props.label}>
+        <label for='input' class='block mb-2 label'>
+          {props.label}
+        </label>
+      </Show>
+      <div class='flex flex-col grow'>
+        <textarea
+          class={`surface-base outline-0 h-24 ring-2 ring-transparent rounded-lg focus:border-blue-500 transition hover:bg-slate-100 dark:hover:bg-slate-800 border-0 block p-2.5 dark:placeholder-gray-400 dark:text-white border ${
+            error()
+              ? 'focus:ring-red-500 dark:focus:ring-red-500'
+              : 'focus:ring-blue-500 dark:focus:ring-blue-500'
+          }`}
+          onInput={(e) => {
+            if (!!props.validator) {
+              if (!props.validator(e.currentTarget.value)) {
+                setError(true);
+                console.info('invalid');
+                return;
+              }
+            }
+            setError(false);
+            if (props.onInput) props.onInput(e.currentTarget.value);
+          }}
+          id='input'
+          required={props.required}
+          value={props.value ?? ''}
+          placeholder={props.placeholder}
+          autocomplete='off'
+          spellcheck={false}
+          readOnly={props.readonly}
+        />
+        <Show when={error()}>
+          <span class='text-sm text-red'>{props.error ?? 'invalid value'}</span>
+        </Show>
+      </div>
+    </div>
+  );
+};
+
+export default Textarea;
+
+interface Prop {
+  onInput?: (e: string) => void;
+  label?: string;
+  placeholder?: string;
+  required?: boolean;
+  value?: string;
+  validator?: (v: string) => boolean;
+  error?: string;
+  readonly?: boolean;
+}
