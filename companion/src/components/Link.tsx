6b5a6c19338d50b3d7c26304e6246616d6eddeb2
Wesley <wes.urb@gmail.com>
2022-12-12 12:23:25 +0100

    general purpose styled link

--- companion/src/components/Link.tsx
+++ companion/src/components/Link.tsx
@@ -1,11 +1,14 @@
 import type { Component } from 'solid-js';
 import { language } from '../helper/store.js';
 import { useStore } from '@nanostores/solid';
-import './styles/LinkStyles.scss';
+
 const Link: Component<Props> = (props) => {
   const $language = useStore(language);
   return (
-    <a class='customLink' href={props.href + $language()}>
+    <a
+      class='tracking-wide text-2xl text-blue-400 hover:underline'
+      href={props.href + $language()}
+    >
       {props.children}
     </a>
   );
