f262f9c1a149850c3655f98b2303b4e44617b23d
wesley <wes.urb@gmail.com>
2022-12-30 22:41:34 +0100

    add slideable setting menu for mobile view

--- extension/src/components/settingToggle.tsx
+++ extension/src/components/settingToggle.tsx
@@ -1,18 +1,36 @@
-import { Component, createEffect, createSignal, Show } from 'solid-js';
+import {
+  Component,
+  createEffect,
+  createSignal,
+  onMount,
+  onCleanup,
+  Show,
+} from 'solid-js';
 import { Setting, ShortcutSetting } from '../../types/settings.js';
 import SettingContent from './settingContent.jsx';
 
 const SettingToggle: Component<Prop> = (props) => {
   let settingPanel: HTMLElement;
   const [showSetting, setShowSetting] = createSignal(false);
+  onMount(() =>
+    window.addEventListener(
+      'resize',
+      () => (settingPanel.style.height = '100%')
+    )
+  );
+  onCleanup(() =>
+    window.removeEventListener(
+      'resize',
+      () => (settingPanel.style.height = '100%')
+    )
+  );
   createEffect(() => {
+    startHeight = settingPanel.clientHeight;
     const handleClickOutside = (event: MouseEvent) => {
       const t = event.target as Node;
       if (!settingPanel || t.nodeName == 'SPAN') return;
       if (settingPanel && !settingPanel.contains(event.target as Node)) {
-        setShowSetting(false);
-        const el = document.getElementById('content');
-        el!.style.transform = '';
+        animateWidgets();
       }
     };
     document.addEventListener('mousedown', handleClickOutside);
@@ -20,6 +38,42 @@ const SettingToggle: Component<Prop> = (props) => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   });
+  let startHeight = 0;
+  let menuClicked = false;
+  const clickstart = (e: PointerEvent) => {
+    menuClicked = true;
+    settingPanel.style.height = settingPanel.clientHeight + 'px';
+    settingPanel.classList.remove('h-full');
+  };
+  const clickend = (e: PointerEvent) => {
+    menuClicked = false;
+    settingPanel.classList.add('h-full');
+  };
+  const clickmove = (e: PointerEvent) => {
+    if (!menuClicked) return;
+    const height = startHeight - e.clientY;
+    if (height < 300 || height >= startHeight) return;
+    settingPanel.style.height = `${height}px`;
+  };
+
+  const animateWidgets = () => {
+    setShowSetting(!showSetting());
+    const el = document.getElementById('content');
+    if (showSetting()) {
+      el!.classList.add(
+        '-translate-y-15',
+        'md:-translate-y-0',
+        'md:-translate-x-15'
+      );
+      el!.style.transition = 'all 300ms';
+    } else
+      el!.classList.remove(
+        '-translate-y-15',
+        'md:-translate-y-0',
+        'md:-translate-x-15'
+      );
+  };
+
   return (
     <>
       <div
@@ -29,14 +83,7 @@ const SettingToggle: Component<Prop> = (props) => {
           class={`i-mdi-cog transition cursor-pointer p-3 transition ease-in-out duration-300 bg-gray-800 dark:bg-gray-200 outline-transparent border-red ${
             showSetting() ? 'rotate-45' : 'hover:rotate-12'
           }`}
-          onClick={() => {
-            setShowSetting(!showSetting());
-            const el = document.getElementById('content');
-            if (showSetting()) {
-              el!.style.transform = 'translate(-10%,0)';
-              el!.style.transition = 'all 300ms';
-            } else el!.style.transform = '';
-          }}
+          onClick={animateWidgets}
         />
       </div>
       <Show when={showSetting()}>
@@ -44,15 +91,28 @@ const SettingToggle: Component<Prop> = (props) => {
       </Show>
       <aside
         ref={settingPanel!}
-        class={`absolute right-0 top-0 h-full w-full md:w-100 max-w-full md:max-w-1/2 surface-base transition duration-300 z-30 overflow-y-auto ${
-          !showSetting() ? 'translate-x-full' : ''
+        class={`absolute right-0 bottom-0 md:top-0 h-full w-full md:w-100 max-w-full md:max-w-1/2 surface-base transition duration-300 z-30 overflow-y-auto ${
+          !showSetting()
+            ? 'translate-y-full md:translate-y-0 md:translate-x-full'
+            : ''
         }`}
+        onpointermove={clickmove}
+        onpointerleave={clickend}
+        onpointerup={clickend}
       >
+        <Show when={showSetting()}>
+          <div
+            class='sticky top-0 md:hidden h-12 w-full z-40 bg-blue/60 dark:bg-blue/40 cursor-row-resize grid place-content-center'
+            onpointerdown={clickstart}
+          >
+            <div class='i-mdi-arrow-split-horizontal bg-white dark:bg-black'></div>
+          </div>
+        </Show>
         <SettingContent
           settings={props.settings}
-          addShortcut={(s) => props.addShortcut(s)}
-          onRefreshImage={() => props.onRefreshImage()}
-          onWeatherUpdate={() => props.onWeatherUpdate()}
+          addShortcut={props.addShortcut}
+          onRefreshImage={props.onRefreshImage}
+          onWeatherUpdate={props.onWeatherUpdate}
         />
       </aside>
     </>
