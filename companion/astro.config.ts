c61214108bc655e242b77ea90138989495b95bd4
Wesley <wes.urb@gmail.com>
2023-01-18 01:00:30 +0100

    update render methode

--- companion/astro.config.ts
+++ companion/astro.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
-import node from '@astrojs/node';
 import Unocss from 'unocss/astro'
 
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   integrations: [solidJs(), Unocss()],
-  adapter: node({ mode: 'standalone' }),
 });
