520b876514add33200d7340eeebf2d793df9fc81
Wesley <wes.urb@gmail.com>
2023-01-18 16:01:34 +0100

    add entrypoint animations

--- companion/src/components/Home.tsx
+++ companion/src/components/Home.tsx
@@ -1,13 +1,24 @@
-import { Component, onCleanup, onMount } from 'solid-js';
-import { AvailableLanguages, messages } from '../lang.js';
+import { useStore } from '@nanostores/solid';
+import { Component, For, onCleanup, onMount } from 'solid-js';
+import { language } from '../helper/store.js';
+import useQuery from '../helper/useQuery.js';
+import {
+  availableLanguages,
+  AvailableLanguages,
+  features,
+  messages,
+} from '../lang.js';
+import Feature from './Feature.jsx';
+import Footer from './Footer.jsx';
 
 import './styles/animations.scss';
 
 const Home: Component = () => {
   const isChrome = navigator.userAgent.includes('Chrome');
-  const language =
-    (location.href.split('/').at(-1) as AvailableLanguages) || 'en';
+  const $lang = useStore(language);
   onMount(() => {
+    const queryLang = useQuery(window.location.href).lang as AvailableLanguages;
+    if (availableLanguages.includes(queryLang)) language.set(queryLang);
     const img = document.querySelector('#tabdash_example') as HTMLImageElement;
     img.addEventListener('mousemove', (e) => animateImage(e, img));
   });
@@ -22,20 +33,24 @@ const Home: Component = () => {
     const centerY = e.clientY - height;
     const degX = centerX * 0.005;
     const degY = centerY * 0.01;
-    element.style.transform = `perspective(1000px) scale(0.8) rotateX(${degX}deg) rotateY(${degY}deg)`;
+    element.style.transform = `perspective(1000px) rotateX(${degX}deg) rotateY(${degY}deg)`;
   };
   return (
-    <main class='flex flex-col h-full items-center gap-4 p-2 md:p-6 lg:p-12'>
-      <span class='text-4xl md:text-6xl p-8 font-bold text-white/40'>
-        {messages.welcome[language]}{' '}
-        <h1 class='text-5xl md:text-7xl text-animation'>TabDash</h1>
-      </span>
-      <div class='h-full flex flex-col justify-around md:grid md:grid-rows-2 md:grid-cols-2 md:grid-rows-1 md:items-center relative'>
-        <section class='w-full p-2 grid place-content-center'>
+    <main class='h-screen overflow-y-auto overflow-x-hidden'>
+      <section class='flex flex-col justify-around items-center h-full'>
+        <div class='text-4xl md:text-6xl m-8 font-bold text-white/40 grid place-content-center'>
+          <span class='animate-[animateIn_500ms]'>
+            {messages.welcome[$lang()]}
+          </span>
+          <span class='animate-[animateIn_500ms_200ms]'>
+            <h1 class='text-5xl md:text-7xl text-animation'>TabDash</h1>
+          </span>
+        </div>
+        <div class='w-full p-2 grid place-content-center animate-[animateIn_500ms_300ms]'>
           {!isChrome && (
-            <div class='flex flex-col justify-center items-center gap-2 md:gap-4'>
+            <div class='flex flex-col justify-center items-center gap-2'>
               <span class='text-xl text-center'>
-                {messages['for firefox'][language]}
+                {messages['for firefox'][$lang()]}
               </span>
               <a href='https://addons.mozilla.org/de/firefox/addon/tabdash/'>
                 <img
@@ -48,21 +63,38 @@ const Home: Component = () => {
           {isChrome && (
             <div>
               <span class='text-xl text-center'>
-                {messages['for chrome'][language]}
+                {messages['for chrome'][$lang()]}
               </span>
             </div>
           )}
-        </section>
-        <section>
+        </div>
+        <div class='p-4 flex justify-center items-center lg:max-h-2/3'>
           <img
             id='tabdash_example'
-            src='/homescreen.png'
+            src='/images/homescreen.png'
             alt='example screen'
             loading='lazy'
-            class='rounded-xl scale-80'
+            height='100px'
+            class='rounded-xl lg:max-h-4/5 lg:max-w-9/10 animate-[animateIn_500ms_400ms]'
           />
-        </section>
-      </div>
+        </div>
+      </section>
+      <section class='flex flex-col justify-center items-between h-auto'>
+        <div class='sticky left-1/2 -translate-x-1/2 top-2 h-20 bg-gray/50 w-64 grid place-content-center tracking-widest font-bold text-xl z-10 backdrop-blur-sm rounded-xl'>
+          Features
+        </div>
+        <div class='w-full flex flex-col items-center'>
+          <For each={features}>
+            {(feature, index) => (
+              <Feature
+                imgSide={index() % 2 == 0 ? 'right' : 'left'}
+                feature={feature}
+              />
+            )}
+          </For>
+        </div>
+      </section>
+      <Footer />
     </main>
   );
 };
