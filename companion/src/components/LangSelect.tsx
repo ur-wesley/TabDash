4fe71787ac78aec67c46f11e6ddd71c21697be75
Wesley <wes.urb@gmail.com>
2023-01-18 01:02:30 +0100

    update language picker logic

--- companion/src/components/LangSelect.tsx
+++ companion/src/components/LangSelect.tsx
@@ -4,24 +4,30 @@ import { language } from '../helper/store.js';
 import { useStore } from '@nanostores/solid';
 
 const LangSelect: Component = () => {
-  const $language = useStore(language);
+  const $lang = useStore(language);
   return (
     <div>
       <select
+        class='p-2 rounded-lg cursor-pointer border-2 border-transparent hover:border-blue-400 transition'
         oninput={(e) => {
           language.set(e.currentTarget.value as AvailableLanguages);
           const el = window.location.pathname.split('/');
-          const index = el.length == 2 ? 1 : 2;
-          console.log(el.length, index);
-          if (el[index] != $language()) {
-            el[index] = $language();
-            window.location.href = el.join('/');
+          if (el[1] == 'docs') {
+            const index = el.length == 2 ? 1 : 2;
+            if (el[index] != $lang()) {
+              el[index] = $lang();
+              window.location.href = el.join('/');
+            }
           }
         }}
       >
         <For each={availableLanguages}>
           {(lang) => (
-            <option selected={lang == $language()} value={lang}>
+            <option
+              class='cursor-pointer'
+              selected={lang == $lang()}
+              value={lang}
+            >
               {lang}
             </option>
           )}
