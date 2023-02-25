7cf40c10cb21c25cac3dcccf2cf1a5726afbcf57
Wesley <wes.urb@gmail.com>
2023-02-25 20:58:13 +0100

    formatting

--- extension/src/components/settings/management.tsx
+++ extension/src/components/settings/management.tsx
@@ -1,17 +1,17 @@
-import { Component, Show } from 'solid-js';
-import { sendToast } from '../../../types/notification.js';
-import { Setting } from '../../../types/settings.js';
-import { settingStore, SettingObject } from '../../api/settingStore.js';
+import { Component, Show } from "solid-js";
+import { sendToast } from "../../../types/notification.js";
+import { Setting } from "../../../types/settings.js";
+import { settingStore, SettingObject } from "../../api/settingStore.js";
 
-import { locale } from '../../App.jsx';
-import { helpLinks, messages } from '../../lang.js';
-import TextButton from '../controls/button.jsx';
-import Categorie from '../controls/categorie.jsx';
-import FileInput from '../controls/fileinput.jsx';
-import Input from '../controls/input.jsx';
-import Sublabel from '../controls/sublabel.jsx';
-import Tabs from '../controls/tabs.jsx';
-import Textarea from '../controls/textarea.jsx';
+import { locale } from "../../App.jsx";
+import { helpLinks, messages } from "../../lang.js";
+import TextButton from "../controls/button.jsx";
+import Categorie from "../controls/categorie.jsx";
+import FileInput from "../controls/fileinput.jsx";
+import Input from "../controls/input.jsx";
+import Sublabel from "../controls/sublabel.jsx";
+import Tabs from "../controls/tabs.jsx";
+import Textarea from "../controls/textarea.jsx";
 
 const Management: Component<Props> = (props) => {
   const getSettingObject = async (json: any): Promise<SettingObject> => {
@@ -23,12 +23,12 @@ const Management: Component<Props> = (props) => {
       const settings = await navigator.clipboard.readText();
       settingStore.set(await getSettingObject(JSON.parse(settings)));
       sendToast(
-        messages['import success clipboard'][locale()],
-        'success',
+        messages["import success clipboard"][locale()],
+        "success",
         5000
       );
     } catch {
-      sendToast(messages['import fail'][locale()], 'error', 5000);
+      sendToast(messages["import fail"][locale()], "error", 5000);
     }
   };
 
@@ -42,27 +42,27 @@ const Management: Component<Props> = (props) => {
         )
       );
       sendToast(
-        messages['export success clipboard'][locale()],
-        'success',
+        messages["export success clipboard"][locale()],
+        "success",
         5000
       );
     } catch {
-      sendToast(messages['export fail'][locale()], 'error', 5000);
+      sendToast(messages["export fail"][locale()], "error", 5000);
     }
   };
 
   const importFromCloud = async () => {
     try {
-      const pw = prompt(messages['select export password'][locale()]);
+      const pw = prompt(messages["select export password"][locale()]);
       if (!pw) return;
       const key = props.settings.id;
       const res = await fetch(
         `${import.meta.env.VITE_COMPANION_BASE}/api/setting/${key}?p=${pw}`
       ).then((r) => r.json());
       if (res) settingStore.set(await getSettingObject(res));
-      sendToast(messages['import success cloud'][locale()], 'success', 5000);
+      sendToast(messages["import success cloud"][locale()], "success", 5000);
     } catch {
-      sendToast(messages['import fail'][locale()], 'error', 5000);
+      sendToast(messages["import fail"][locale()], "error", 5000);
     }
   };
 
@@ -71,38 +71,38 @@ const Management: Component<Props> = (props) => {
       const data = JSON.parse(value);
       settingStore.set(await getSettingObject(data));
       sendToast(
-        messages['import success clipboard'][locale()],
-        'success',
+        messages["import success clipboard"][locale()],
+        "success",
         5000
       );
     } catch (error) {
-      sendToast(messages['import fail'][locale()], 'error', 5000);
+      sendToast(messages["import fail"][locale()], "error", 5000);
     }
   };
 
   const exportToCloud = async () => {
     try {
-      const pw = prompt(messages['import password'][locale()]);
+      const pw = prompt(messages["import password"][locale()]);
       if (!pw) return;
       const key =
-        props.settings.id == '0' ? crypto.randomUUID() : props.settings.id;
-      settingStore.setKey('id', key);
+        props.settings.id == "0" ? crypto.randomUUID() : props.settings.id;
+      settingStore.setKey("id", key);
       await fetch(
         `${import.meta.env.VITE_COMPANION_BASE}/api/setting/${key}?p=${pw}`,
         {
-          method: 'POST',
+          method: "POST",
           body: JSON.stringify({ ...props.settings, cache: { images: [] } }),
         }
       );
-      sendToast(messages['export success cloud'][locale()], 'success', 5000);
+      sendToast(messages["export success cloud"][locale()], "success", 5000);
     } catch {
-      sendToast(messages['export fail'][locale()], 'error', 5000);
+      sendToast(messages["export fail"][locale()], "error", 5000);
     }
   };
 
   const exportToFile = async () => {
     const data =
-      'text/json;charset=utf-8,' +
+      "text/json;charset=utf-8," +
       encodeURIComponent(
         JSON.stringify(
           { ...props.settings, cache: { images: [] } },
@@ -110,15 +110,15 @@ const Management: Component<Props> = (props) => {
           2
         )
       );
-    const a = document.createElement('a');
-    a.href = 'data:' + data;
-    a.download = 'TabDash.json';
+    const a = document.createElement("a");
+    a.href = "data:" + data;
+    a.download = `TabDash-${new Date().toLocaleDateString(locale())}.json`;
     a.click();
     a.remove();
   };
 
   const resetAll = () => {
-    const doReset = confirm(messages['confirm reset'][locale()]);
+    const doReset = confirm(messages["confirm reset"][locale()]);
     if (!doReset) return;
     props.reset();
   };
@@ -132,13 +132,13 @@ const Management: Component<Props> = (props) => {
         <div>
           <Sublabel label={messages.local[locale()]} />
           <FileInput
-            label={messages['drop file'][locale()]}
+            label={messages["drop file"][locale()]}
             oninput={(v) => {
               importFromInput(v);
             }}
           />
           <Input
-            label={messages['manually setting input'][locale()]}
+            label={messages["manually setting input"][locale()]}
             validator={(v) => {
               if (!v) return true;
               try {
@@ -148,31 +148,31 @@ const Management: Component<Props> = (props) => {
                 return false;
               }
             }}
-            error={messages['no json format'][locale()]}
+            error={messages["no json format"][locale()]}
             onInput={importFromInput}
           />
           <Show when={false}>
             <TextButton onClick={importFromClipboard}>
-              {messages['import local'][locale()]}
+              {messages["import local"][locale()]}
             </TextButton>
           </Show>
           <Sublabel label={messages.online[locale()]} />
-          <div class='flex flex-col w-full gap-2'>
+          <div class="flex flex-col w-full gap-2">
             <Input
-              label={messages['setting id'][locale()]}
+              label={messages["setting id"][locale()]}
               value={props.settings.id}
               onInput={(e: string) => {
-                settingStore.setKey('id', e);
+                settingStore.setKey("id", e);
               }}
               validator={(e: string) => {
                 const validId =
                   /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
                 return new RegExp(validId).test(e);
               }}
-              error={messages['incorrect id'][locale()]}
+              error={messages["incorrect id"][locale()]}
             />
             <TextButton onClick={importFromCloud}>
-              {messages['import cloud'][locale()]}
+              {messages["import cloud"][locale()]}
             </TextButton>
           </div>
         </div>
@@ -182,40 +182,38 @@ const Management: Component<Props> = (props) => {
             readonly={true}
             value={JSON.stringify(props.settings, undefined, 2)}
           />
-          <div class='flex justify-around my-4'>
+          <div class="flex justify-around my-4">
             <TextButton onClick={exportToFile}>
-              {messages['save to file'][locale()]}
+              {messages["save to file"][locale()]}
             </TextButton>
             <TextButton
-              onClick={() => {
-                exportToClipboard();
-              }}
+              onClick={exportToClipboard}
             >
-              {messages['export local'][locale()]}
+              {messages["export local"][locale()]}
             </TextButton>
           </div>
           <Sublabel label={messages.online[locale()]} />
-          <div class='flex flex-col w-full gap-2'>
+          <div class="flex flex-col w-full gap-2">
             <Input
-              label={messages['setting id'][locale()]}
+              label={messages["setting id"][locale()]}
               value={props.settings.id}
               onInput={(e: string) => {
-                settingStore.setKey('id', e);
+                settingStore.setKey("id", e);
               }}
               validator={(e: string) => {
                 const validId =
                   /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
                 return new RegExp(validId).test(e);
               }}
-              error={messages['incorrect id'][locale()]}
+              error={messages["incorrect id"][locale()]}
             />
             <TextButton onClick={exportToCloud}>
-              {messages['export cloud'][locale()]}
+              {messages["export cloud"][locale()]}
             </TextButton>
           </div>
         </div>
       </Tabs>
-      <div class='flex flex-col justify-center mt-8'>
+      <div class="flex flex-col justify-center mt-8">
         <TextButton background={true} onClick={resetAll}>
           {messages.reset[locale()]}
         </TextButton>
