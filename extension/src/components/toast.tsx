38c0f26b581cf719517d9d5314f998952af2868b
Wesley <wes.urb@gmail.com>
2023-02-25 21:04:02 +0100

    add animation

--- extension/src/components/toast.tsx
+++ extension/src/components/toast.tsx
@@ -1,31 +1,45 @@
-import { Component, createEffect, createSignal, onCleanup } from 'solid-js';
-import { Notification, NotificationType } from '../../types/notification.js';
+import { Component, createEffect, createSignal, onCleanup } from "solid-js";
+import { Notification, NotificationType } from "../../types/notification.js";
 
 const Toast: Component<Prop> = (props) => {
   const notificationStyle = (type: NotificationType): string => {
     switch (type) {
-      case 'success':
-        return 'bg-green-400/20 text-black';
-      case 'error':
-        return 'bg-red-400/20 text-black';
-      case 'info':
-        return 'bg-blue-400/20 text-black';
+      case "success":
+        return "bg-green-400/20 text-black";
+      case "error":
+        return "bg-red-400/20 text-black";
+      case "info":
+        return "bg-blue-400/20 text-black";
       default:
-        return 'bg-blue-400/20 text-black';
+        return "bg-blue-400/20 text-black";
     }
   };
 
   const barStyle = (type: NotificationType): string => {
     switch (type) {
-      case 'success':
-        return 'bg-green-800';
-      case 'error':
-        return 'bg-red-800';
-      case 'info':
-        return 'bg-blue-800';
+      case "success":
+        return "bg-green-800";
+      case "error":
+        return "bg-red-800";
+      case "info":
+        return "bg-blue-800";
       default:
-        return 'bg-blue-800';
+        return "bg-blue-800";
     }
+  };
+
+  let toastEl: HTMLDivElement;
+  const removeElement = () => {
+    const slideRight = [
+      { opacity: "1", transform: "translateX(0)" },
+      { opacity: "0.5", transform: "translateX(100%)" },
+    ];
+    const timing = {
+      duration: 500,
+      iterations: 1,
+    }
+    const animation = toastEl.animate(slideRight, timing);
+    animation.onfinish = (() => props.remove(props.notification.id));
   };
 
   let intervall: any;
@@ -34,11 +48,11 @@ const Toast: Component<Prop> = (props) => {
   );
   createEffect(() => {
     width = 100;
-    intervall = setInterval(() => {
+    intervall = setInterval(async () => {
       if (duration() > 0) setDuration(duration() - 10);
       else {
-        props.remove(props.notification.id);
         clearInterval(intervall);
+        removeElement();
       }
     }, 10);
   });
@@ -50,14 +64,15 @@ const Toast: Component<Prop> = (props) => {
   };
   return (
     <div
+      ref={toastEl!}
       class={`${notificationStyle(
         props.notification.type
       )} backdrop-blur-md top-0 rounded-0 md:rounded-lg relative w-full md:w-64 flex-wrap flex flex-col overflow-hidden self-end`}
     >
       <span class="p-2">{props.notification.msg}</span>
       <button
-        class='border-none bg-transparent absolute top-0 right-0 cursor-pointer p-1'
-        onclick={() => props.remove(props.notification.id)}
+        class="border-none bg-transparent absolute top-0 right-0 cursor-pointer p-1"
+        onclick={removeElement}
       >
         x
       </button>
