ac3e4059aec81deec0a627815b7ac4a985e7efdd
Wesley <wes.urb@gmail.com>
2023-02-27 09:51:08 +0100

    add mobile menu

--- extension/src/components/settingToggle.tsx
+++ extension/src/components/settingToggle.tsx
@@ -5,72 +5,90 @@ import {
   onMount,
   onCleanup,
   Show,
-} from 'solid-js';
-import { Setting, ShortcutSetting } from '../../types/settings.js';
-import SettingContent from './settingContent.jsx';
+} from "solid-js";
+import { Setting, ShortcutSetting } from "../../types/settings.js";
+import SettingContent from "./settingContent.jsx";
 
 const SettingToggle: Component<Prop> = (props) => {
   let settingPanel: HTMLElement;
   const [showSetting, setShowSetting] = createSignal(false);
   onMount(() =>
     window.addEventListener(
-      'resize',
-      () => (settingPanel.style.height = '100%')
+      "resize",
+      () => (settingPanel.style.height = "100%")
     )
   );
   onCleanup(() =>
     window.removeEventListener(
-      'resize',
-      () => (settingPanel.style.height = '100%')
+      "resize",
+      () => (settingPanel.style.height = "100%")
     )
   );
+  let startHeight = 0;
+  let menuHeight = 0;
+  let menuClicked = false;
   createEffect(() => {
     startHeight = settingPanel.clientHeight;
     const handleClickOutside = (event: MouseEvent) => {
       const t = event.target as Node;
-      if (!settingPanel || t.nodeName == 'SPAN') return;
+      if (!settingPanel || t.nodeName == "SPAN") return;
       if (settingPanel && !settingPanel.contains(event.target as Node)) {
         animateWidgets(false);
       }
     };
-    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   });
-  let startHeight = 0;
-  let menuClicked = false;
-  const clickstart = (e: PointerEvent) => {
+  //#region menu move handler
+  const dragstart = (e: TouchEvent | MouseEvent) => {
+    e.preventDefault();
     menuClicked = true;
-    settingPanel.style.height = settingPanel.clientHeight + 'px';
-    settingPanel.classList.remove('h-full');
+    settingPanel.classList.remove("h-full");
+    document.addEventListener("pointermove", dragmove);
+    document.addEventListener("pointerup", dragend);
   };
-  const clickend = (e: PointerEvent) => {
+  const dragend = (e: TouchEvent | MouseEvent) => {
+    e.preventDefault();
     menuClicked = false;
-    settingPanel.classList.add('h-full');
+    document.removeEventListener("pointermove", dragmove);
+    document.removeEventListener("pointerup", dragend);
+    if (startHeight - menuHeight < 300) {
+      setShowSetting(false);
+      animateWidgets(false);
+      setTimeout(() => {
+        menuHeight = 0;
+        settingPanel.style.height = `${startHeight}px`;
+      }, 500)
+    }
   };
-  const clickmove = (e: PointerEvent) => {
+  const dragmove = (e: MouseEvent) => {
+    e.preventDefault();
     if (!menuClicked) return;
-    const height = startHeight - e.clientY;
-    if (height < 300 || height >= startHeight) return;
-    settingPanel.style.height = `${height}px`;
+    menuHeight += e.movementY;
+    if (startHeight - menuHeight >= startHeight)
+      menuHeight = 0;
+    settingPanel.style.height = `${startHeight - menuHeight}px`;
+
   };
+  //#endregion
 
   const animateWidgets = (state: boolean = true) => {
     setShowSetting(state);
-    const el = document.getElementById('content');
+    const el = document.getElementById("content");
     if (showSetting()) {
       el!.classList.add(
-        '-translate-y-15',
-        'md:-translate-y-0',
-        'md:-translate-x-15'
+        "-translate-y-15",
+        "md:-translate-y-0",
+        "md:-translate-x-15"
       );
-      el!.style.transition = 'all 300ms';
+      el!.style.transition = "all 300ms";
     } else
       el!.classList.remove(
-        '-translate-y-15',
-        'md:-translate-y-0',
-        'md:-translate-x-15'
+        "-translate-y-15",
+        "md:-translate-y-0",
+        "md:-translate-x-15"
       );
   };
 
@@ -80,32 +98,27 @@ const SettingToggle: Component<Prop> = (props) => {
         class={`z-40 widget absolute right-2 bottom-2 grid place-content-center overflow-hidden p-1`}
       >
         <span
-          class={`i-mdi-cog transition cursor-pointer p-3 transition ease-in-out duration-300 bg-gray-800 dark:bg-gray-200 outline-transparent border-red ${
-            showSetting() ? 'rotate-45' : 'hover:rotate-12'
-          }`}
+          class={`i-mdi-cog transition cursor-pointer p-3 transition ease-in-out duration-300 outline-transparent border-red ${showSetting() ? "rotate-45 text-gray-800 dark:text-gray-200" : "hover:rotate-12"
+            }`}
           onClick={() => animateWidgets(!showSetting())}
         />
       </div>
       <Show when={showSetting()}>
-        <div class='absolute top-0 left-0 w-screen h-screen backdrop-brightness-80'></div>
+        <div class="absolute top-0 left-0 w-screen h-screen backdrop-brightness-90"></div>
       </Show>
       <aside
         ref={settingPanel!}
-        class={`absolute right-0 bottom-0 md:top-0 h-full w-full md:w-100 max-w-full md:max-w-1/2 surface-base transition duration-300 z-30 overflow-y-auto ${
-          !showSetting()
-            ? 'opacity-0 translate-y-full md:translate-y-0 md:translate-x-full'
-            : 'opacity-100'
-        }`}
-        onpointermove={clickmove}
-        onpointerleave={clickend}
-        onpointerup={clickend}
+        class={`absolute right-0 bottom-0 md:top-0 h-full w-full md:w-100 max-w-full md:max-w-1/2 surface-base transition z-30 overflow-y-auto ${!showSetting()
+          ? "opacity-0 translate-y-full md:translate-y-0 md:translate-x-full"
+          : "opacity-100"
+          }`}
       >
         <Show when={showSetting()}>
           <div
-            class='sticky top-0 md:hidden h-12 backdrop-blur-sm w-full z-40 bg-blue/60 dark:bg-blue/40 cursor-row-resize grid place-content-center'
-            onpointerdown={clickstart}
+            class="sticky top-0 md:hidden h-12 backdrop-blur-sm w-full z-40 bg-blue/60 dark:bg-blue/40 cursor-row-resize grid place-content-center"
+            onpointerdown={dragstart}
           >
-            <div class='i-mdi-arrow-split-horizontal bg-white dark:bg-black'></div>
+            <div class="i-mdi-arrow-split-horizontal bg-white dark:bg-black"></div>
           </div>
         </Show>
         <SettingContent
